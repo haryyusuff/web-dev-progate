1623a686815b59031a1b93f529b3af8316286ddb
Hary Yusuf <haryyusuf11@gmail.com>
2022-05-22 19:51:06 +0700

    Update script.js

--- project/script.js
+++ project/script.js
@@ -3,11 +3,29 @@ const calculatorScreen = document.querySelector(".calculator-screen")
 const updateScreen = (number) => {
     calculatorScreen.value = number 
 }
-
+const inputNumber = (number) => {
+    if (currentNumber === '0') {
+        currentNumber = number
+    } else {
+        currentNumber += number
+    }
+}
 const numbers = document.querySelectorAll(".number")
 
 numbers.forEach ((number) => {
     number.addEventListener("click", (event) => {
-        updateScreen(event.target.value);
+        inputNumber(event.target.value);
+        updateScreen(currentNumber)
     })
 })
+
+const oeprators = document.querySelectorAll(".operator")
+
+oeprators.forEach((operator) => {
+    operatort.addEventListener("click", (event) => {
+        
+    })
+})
+let prevNumber = ''
+let calculationOperator = ''
+let currentNumber = '0'
